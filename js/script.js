7cb2a55a7e2864819caad6c7b2fb13be2b93806c
Joan Clarke <joanclarke100@gmail.com>
2019-10-06 23:35:32 -0500

    create display functionality on calculator screen

--- js/script.js
+++ js/script.js
@@ -1,10 +1,37 @@
 class Calculator {
-  constructor(previousOperandTextElement, currentOperandTextElement){
+  constructor(previousOperandTextElement, currentOperandTextElement) {
     this.previousOperandTextElement = previousOperandTextElement
     this.currentOperandTextElement = currentOperandTextElement
+    // this.clear()
   }
-}
+  clear() {
+    this.currentOperand = ''
+    this.previousOperand = ''
+    this.operation = undefined
+  }
 
+  delete(){
+
+  }
+
+  appendNumber(number) {
+    if (number === '.' && this.currentOperand.includes('.')) return
+    this.currentOperand = this.currentOperand.toString() + number.toString()
+  }
+
+  chooseOperation(operation){
+
+  }
+
+  compute(){
+
+  }
+
+  updateDisplay(){
+    this.currentOperandTextElement.innertext = this.currentOperand;
+  }
+
+}
 
 const numberButtons = document.querySelectorAll('[data-number]');
 const operationsButtons = document.querySelectorAll('[data-operation]');
@@ -13,3 +40,12 @@ const deleteButton = document.querySelector('[data-delete]');
 const allClearButton = document.querySelector('[data-all-clear]');
 const previousOperandTextElement = document.querySelector('[previous-operand]');
 const currentOperandTextElement = document.querySelector('[current-operand]');
+
+const calculator = new Calculator(previousOperandTextElement, currentOperandTextElement)
+
+numberButtons.forEach(button => {
+  button.addEventListener('click', () => {
+    calculator.appendNumber(button.innerText)
+    calculator.updateDisplay()
+  })
+})
